2d95d8c386cb7653e680e292ff16f3e2ba070195
cliffordkleinsr <cnjoroge925@gmail.com>
2025-04-04 16:29:37 +0300

    use produce

--- src/routes/(protected)/backend/index.tsx
+++ src/routes/(protected)/backend/index.tsx
@@ -1,4 +1,5 @@
 import { createAsync, query, redirect } from "@solidjs/router";
+import { produce } from "solid-js/store";
 import { getSession } from "~/apis/auth";
 import { FlashContextType, useFlashContext } from "~/context/FlashContext";
 
@@ -20,8 +21,10 @@ export default function backend() {
       <p>{flashstore?.flash.message}</p>
       <p>{flashstore?.flash.type}</p>
       <button class='btn' onClick={() => {
-        flashstore?.setFlash('message', 'Bye Bye')
-        flashstore?.setFlash('type', 'warning')
+        flashstore?.setFlash(produce((flash) => {
+          flash.message = 'Bye Bye',
+          flash.type = 'warning'
+        }))
       }}>Gday</button>
     </>
   );
